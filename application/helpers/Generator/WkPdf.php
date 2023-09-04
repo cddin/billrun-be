a1c09183099bf5e4fb58e7b93e7e49037f3a055a
Cddin <cddin80@gmail.com>
2023-09-05 03:19:47 +0800

    test

--- application/helpers/Generator/WkPdf.php
+++ application/helpers/Generator/WkPdf.php
@@ -393,6 +393,8 @@ class Generator_WkPdf extends Billrun_Generator_Pdf {
 
 		Billrun_Factory::log($html);
 
+		$html = "/shared/container/files/invoices//202309/html/202309_1_0.html";
+
 		
 		
 		$pubroot = '/home/ubuntu/billrunbe/public/invoice.html';
